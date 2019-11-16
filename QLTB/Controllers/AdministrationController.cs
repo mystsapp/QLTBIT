ebc09e23f8a4054850293929e4a498189b203a21
ST-PC\ST <vanhongit@gmail.com>
2019-11-16 13:37:49 +0700

    comit

---
+++ QLTB/Controllers/AdministrationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QLTB.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
